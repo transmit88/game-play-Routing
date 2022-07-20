5722861f6d5e30ab26bd883049dffea2df8e0d76
Asen Uzunov <transmit1988@abv.bg>
2022-07-20 14:46:53 +0300

    Add router and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Catalog from './componnents/Catalog/Catalog';
+import CreateGame from './componnents/CreateGame/CreateGame';
 
 import Header from './componnents/Header/Header';
 import Home from './componnents/Home/Home';
-
-import './App.css';
+import Login from './componnents/Login/Login';
+import Register from './componnents/Register/Register';
 
 function App() {
     return (
@@ -16,6 +19,10 @@ function App() {
                 <Routes>
                     
                     <Route path='/' element={<Home />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                    <Route path='/register' element={<Register />}></Route>
+                    <Route path='/create' element={<CreateGame />}></Route>
+                    <Route path='/catalog' element={<Catalog />}></Route>
                 </Routes>
 
 
