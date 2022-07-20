0a0449efb3448345c85dbeeba1294b26ca485ca2
Asen Uzunov <transmit1988@abv.bg>
2022-07-20 13:08:11 +0300

    List latest games from server

---
+++ src/componnents/Home/Home.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import * as gameService from '../../services/gameService';
+import LatestGame from "./LatestGame/LatestGame";
+
+const Home = () => {
+    const [games, setGame] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGame(result);
+            });
+    }, []);
+
+    return (
+        <section id="welcome-world">
+            <div className="welcome-message">
+                <h2>ALL new games are</h2>
+                <h3>Only in GamesPlay</h3>
+            </div>
+            <img src="./images/four_slider_img01.png" alt="hero" />
+            <div id="home-page">
+                <h1>Latest Games</h1>
+                {/* Display div: with information about every game (if any) */}
+
+                {games.map(x => <LatestGame game={x}/>)}
+
+                {/* Display paragraph: If there is no games  */}
+                <p className="no-articles">No games yet</p>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
---
+++ src/services/gameService.js
@@ -0,0 +1,7 @@
+const baseUrl = 'http://localhost:3030';
+
+export const getAll = () => {
+    return fetch(`${baseUrl}/data/games?sortBy=_createdOn%20desc&distinct=category`)
+        .then(res => res.json())
+
+};
